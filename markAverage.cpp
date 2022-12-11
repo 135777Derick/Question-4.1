8200000e46faabc0264f81813a1cd9179d577ef6
135777Derick <117374653+135777Derick@users.noreply.github.com>
2022-12-11 12:57:12 +0300

    Create markAverage.cpp

---
+++ markAverage.cpp
@@ -0,0 +1,23 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+double sum, average;
+double markAverage() {
+    double marks [7];
+    cout<<"Enter the Marks: ";
+    cin>>nums[0];
+    cin>>nums[1];
+    cin>>nums[2];
+    cin>>nums[3];
+    cin>>nums[4];
+    cin>>nums[5];
+    cin>>nums[6];
+}
+    int main(){
+        for(double nums = 0; nums < 6; nums++){
+            sum = num[0] + num[1] +num[2]+num[3]+num[4]+num[5];
+            average = sum/7;
+            cout<<"the Average marks is: "<<average;
+    }
+    return 0;
+}
